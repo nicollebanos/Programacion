bf431608711efcd0e9bf5b3de632e46ead7eb434
nicollebanos <nicolleb2804@gmail.com>
2021-02-11 07:43:31 -0500

    subiendo

--- Clases/clase1.py
+++ Clases/clase1.py
@@ -3,7 +3,12 @@ edad = 19
 estatura = 1.57
 
 
-
+# Generenado saludo
+print("-"*5, "saludo", "-"*5)
 print(Saludo)
+# Dando edad
+print("-"*5, "edad", "-"*5)
 print(edad)
+# Estatura persona
+print("-"*5, "estatura", "-"*5)
 print(estatura)
