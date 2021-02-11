2b35030c8e3850ed906ccc69cdf8ff5276a4fc52
nicollebanos <nicolleb2804@gmail.com>
2021-02-11 07:39:12 -0500

    operaciones

---
+++ Clases/operaciones.py
@@ -0,0 +1,19 @@
+numeroA = 50
+numeroB = 30
+# Suma
+print("-"*5, "suma", "-"*5)
+sumar = numeroA + numeroB
+print("el resultado de la suma es", sumar)
+print(f"la suma dio {sumar} exitosamente")
+# resta 
+print("-"*5, "resta", "-"*5)
+resta = numeroA - numeroB
+print(f"la suma dio {resta} exitosamente")
+# multiplicar 
+print("-"*5, "multiplicación", "-"*5)
+multiplicar = numeroA * numeroB
+print(f"la multiplicación dio {multiplicar} exitosamente")
+# división
+print("-"*5, "división", "-"*5)
+dividir = numeroA / numeroB
+print(f"la división dio {dividir} exitosamente")
