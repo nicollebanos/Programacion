248df47249941b3da715d5e88ffed39a5415501c
nicollebanos <nicolleb2804@gmail.com>
2021-02-13 12:44:30 -0500

    trabajo

---
+++ Talleres/taller1.py
@@ -0,0 +1,11 @@
+Saludo = "Bienvenid@"
+# Generenado saludo
+print("-"*5, "saludo", "-"*5)
+print(Saludo)
+
+# Entrega de datos
+print("-"*5, "número", "-"*5)
+numeroA = input("Digita un número: ")
+numeroB = input("Digite otro número: ")
+print(f"los números dados son {numeroA} y {numeroB}")
+
