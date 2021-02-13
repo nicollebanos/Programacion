3f8f318739742281865ff701dd6d4540fbcfe392
nicollebanos <nicolleb2804@gmail.com>
2021-02-13 13:11:27 -0500

    trabajo

--- Talleres/taller1.py
+++ Talleres/taller1.py
@@ -5,8 +5,8 @@ print(Saludo)
 
 # Entrega de datos
 print("-"*5, "número", "-"*5)
-numeroA = input("Digita un número: ")
-numeroB = input("Digite otro número: ")
+numeroA = input("Digita un número entero: ")
+numeroB = input("Digite otro número entero: ")
 numA = int (numeroA)
 numB = int (numeroB)
 
@@ -14,3 +14,13 @@ numB = int (numeroB)
 print("-"*5, "suma", "-"*5)
 sumar = numA + numB
 print(f"la suma dio {sumar} exitosamente")
+
+# Resta de datos
+print("-"*5, "resta", "-"*5)
+resta = numA - numB
+print(f"la resta dio {resta} exitosamente")
+
+# Mutiplicación de datos
+print("-"*5, "multiplicación", "-"*5)
+multiplicar = numA * numB
+print(f"la Multiplicación dio {multiplicar} exitosamente")
