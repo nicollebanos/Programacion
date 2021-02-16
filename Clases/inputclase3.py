6b0789c26e5e0c21b20072b894b4ad6344a212c8
nicollebanos <nicolleb2804@gmail.com>
2021-02-16 06:51:39 -0500

    input

---
+++ Clases/inputclase3.py
@@ -0,0 +1,7 @@
+#----------------- constatnte -----------------#
+PREGUNTA_NOMBRE = "Como te llamas?: "
+PREGUNTA_EDAD = "Cuantos años tienes?: "
+#----------------- entrada de códigos -----------------#
+
+nombre = input(PREGUNTA_NOMBRE)
+print(f"un gusto en conocerte {nombre}")
