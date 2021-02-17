8ef8a57395d015ae6a18b76e3e40054565a2ecfc
nicollebanos <nicolleb2804@gmail.com>
2021-02-16 21:38:45 -0500

    f

---
+++ Talleres/taller2.py
@@ -0,0 +1,43 @@
+saludo = "Bienvenid@"
+# Generenado saludo
+PREGUNTA_NOMBRE = "Como te llamas?: "
+print("-"*5, "saludo", "-"*5)
+nombre = input(PREGUNTA_NOMBRE)
+print(f"{saludo} {nombre}")
+
+# Entrega de datos
+print("-"*5, "número", "-"*5)
+numeroA = input("Digita un número: ")
+numeroB = input("Digite otro número: ")
+numA = float (numeroA)
+numB = float (numeroB)
+
+# Suma de datos
+print("-"*5, "suma", "-"*5)
+sumar = numA + numB
+print(f"la suma dio {sumar} exitosamente")
+
+# Resta de datos
+print("-"*5, "resta", "-"*5)
+resta = numA - numB
+print(f"la resta dio {resta} exitosamente")
+
+# Mutiplicación de datos
+print("-"*5, "multiplicación", "-"*5)
+multiplicar = numA * numB
+print(f"la multiplicación dio {multiplicar} exitosamente")
+
+# División de datos
+print("-"*5, "división", "-"*5)
+dividir = numA / numB
+print(f"la división dio {dividir} exitosamente")
+
+#Calculando que número es mayor
+print("-"*5, "orden números", "-"*5)
+if numA < numB: 
+    print("El número mayor es:", numB)
+elif numA > numB:
+    print("El número mayor es:", numA)
+elif numA == numB:
+    print("Los números son iguales")
+
