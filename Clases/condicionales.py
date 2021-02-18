15437a424d35aefa0e9041e2371529b600ddc644
nicollebanos <nicolleb2804@gmail.com>
2021-02-18 06:49:31 -0500

    f

---
+++ Clases/condicionales.py
@@ -0,0 +1,9 @@
+#--------- Constantes -----------#
+MENSAJE_BIENVENIDA = "Hola, espero que estes bien"
+PREGUNTA_NUMEROA = "Ingrese un número A"
+PREGUNTA_NUMEROB = "Ingrese un número B"
+
+#--------- Entrada de código -----------#
+print(MENSAJE_BIENVENIDA)
+numeroA = int(input(PREGUNTA_NUMEROA))
+numeroB = int(input(PREGUNTA_NUMEROB))
