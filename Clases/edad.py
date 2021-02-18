af0f1a8a1fef1a97b93fb1fbedd9871191e59084
nicollebanos <nicolleb2804@gmail.com>
2021-02-18 07:18:01 -0500

    F

---
+++ Clases/edad.py
@@ -0,0 +1,14 @@
+#---------- Constantes -----------#
+MENSAJE_BIENVENIDA = " Bienvenid@ la base de datos"
+MENSAJE_MAYOR_EDAD = "Eres mayor de edad, puedes seguir"
+MENSAJE_MENOR_EDAD = "Eres menor de edad, no puedes seguir"
+PREGUNTA_EDAD = "Cuantos años tienes?: "
+
+#---------- Entrada -----------#
+print(MENSAJE_BIENVENIDA)
+edad = int(input(PREGUNTA_EDAD))
+isMayor = edad >= 18
+if(isMayor):
+    print(MENSAJE_MAYOR_EDAD)
+else:
+    print(MENSAJE_MENOR_EDAD)
