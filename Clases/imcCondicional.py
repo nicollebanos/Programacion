e8074c0f9d1a84dd101263c01e5e835476b7c861
nicollebanos <nicolleb2804@gmail.com>
2021-02-18 07:29:22 -0500

    f

--- Clases/imcCondicional.py
+++ Clases/imcCondicional.py
@@ -15,4 +15,15 @@ estatura = float (input(PREGUNTA_ESTATURA))
 imc = peso/(estatura**2)
 isBajoPeso = imc < 18.5 
 isNormal = imc >= 18.5 and imc < 25
-
+isSobrePeso = imc > 25 and imc < 30
+resultado = ""
+if(isBajoPeso):
+    resultado = MENSAJE_BAJO_PESO
+elif(isNormal):
+    resultado = MENSAJE_NORMAL
+elif(isSobrePeso):
+    resultado = MENSAJE_SOBRE_PESO
+else:
+    resultado = MENSAJE_OBESO
+print(MENSAJE_DESPEDIDA, imc)
+print(resultado)
