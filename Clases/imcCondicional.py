7caee00d5c23ae87aad44a890d4278377275271e
nicollebanos <nicolleb2804@gmail.com>
2021-02-18 07:31:01 -0500

    f

--- Clases/imcCondicional.py
+++ Clases/imcCondicional.py
@@ -2,7 +2,7 @@
 PREGUNTA_PESO = "Cuanto pesas en kg?: "
 PREGUNTA_ESTATURA = "Cuanto mides en metros?: "
 MENSAJE_BIENVE = "Hola, como estas? Voy a calcular tu IMC"
-MENSAJE_DESPEDIDA = "Tú IMC es... {}%"
+MENSAJE_DESPEDIDA = "Tú IMC es... {}% "
 MENSAJE_BAJO_PESO = "Come ombe, estas delgado"
 MENSAJE_NORMAL = "Estas en forma"
 MENSAJE_SOBRE_PESO = "Abre el ojo, estas en sobre peso"
