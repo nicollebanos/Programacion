4b2b92b24f07fe073ab85df17ca74ed354b4e53a
nicollebanos <nicolleb2804@gmail.com>
2021-02-18 06:57:54 -0500

    F

--- Clases/condicionales.py
+++ Clases/condicionales.py
@@ -1,9 +1,21 @@
 #--------- Constantes -----------#
 MENSAJE_BIENVENIDA = "Hola, espero que estes bien"
-PREGUNTA_NUMEROA = "Ingrese un número A"
-PREGUNTA_NUMEROB = "Ingrese un número B"
+PREGUNTA_NUMEROA = "Ingrese un número A: "
+PREGUNTA_NUMEROB = "Ingrese un número B: "
+MENSAJE_MAYOR = "El número A es mayor que B"
+MENSAJE_MENOR = "El número A es menor que B"
+MENSAJE_IGUAL = "El número A es igual a B"
 
 #--------- Entrada de código -----------#
 print(MENSAJE_BIENVENIDA)
 numeroA = int(input(PREGUNTA_NUMEROA))
 numeroB = int(input(PREGUNTA_NUMEROB))
+isMayor = numeroA > numeroB
+isMenor = numeroA < numeroB
+
+if (isMayor): 
+    print(MENSAJE_MAYOR)
+elif (isMenor):
+    print(MENSAJE_MENOR)
+else:
+    print(MENSAJE_IGUAL)
