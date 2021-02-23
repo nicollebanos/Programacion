3b7d5ae6169b2a969aed45197229db53b98477a7
nicollebanos <nicolleb2804@gmail.com>
2021-02-22 21:26:19 -0500

    trabajo1

---
+++ Talleres/Taller.Condicional/taller1.py/primero.py
@@ -0,0 +1,27 @@
+# Dos números - mayor - iguales
+
+#Constantes
+MENSAJE_BIENVENIDA = "Buenas, espero que te encuentres muy bien"
+MENSAJE_SALIDA = "Muchras gracias, vuelva pronto"
+PREGUNTA_NOMBRE = "Cúal es tu nombre?: "
+NUMERO1 = "Digite un número: "
+NUMERO2 = "Para continuar, digite otro número: "
+
+print("---------------Bienvenida---------------------")
+nombre = input(PREGUNTA_NOMBRE) 
+print(MENSAJE_BIENVENIDA, nombre)
+print("----------------números----------------------")
+# Datos
+numeroA = input(NUMERO1)
+numeroB = input(NUMERO2)
+numA = float(numeroA)
+numB = float(numeroB)
+
+print("----------------resultado----------------------")
+#Códigos
+if (numA > numB):
+    print("El número ", numA, " es mayor que ", numB)
+elif (numA < numB):
+    print("El número ", numA, " es menor que ",numB)
+else:
+    print("Ambos números son iguales:", numA)
