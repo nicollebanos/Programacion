5dcef5d06ed5e92c0682d77d0389399a5e3669b9
nicollebanos <nicolleb2804@gmail.com>
2021-02-25 07:26:41 -0500

    trabajo

--- Clases/juego1.py
+++ Clases/juego1.py
@@ -1,2 +1,19 @@
 #----------- Entradas ----------#
-MENSAJE_SALUDO =  "Bienvenido a este programa, juguemos!!"
+MENSAJE_SALUDO =  '''Bienvenido 
+                    a este programa,
+                    juguemos!!'''
+PREGUNTA_NUMERO = '''
+                    En este juego debes asertar un número entero
+                    que va desde el 1-10, la idea es que
+                    lo puedes intentar las veces que 
+                    quieras...
+                    muchos exitos, ingresa tu número '''
+PREGUNTA_FALLASTE = 'Ahhhhhhhh! fallaste :3, ingresa otro número'
+MENSAJE_DESPEDIDA = 'Felicidades, ganaste!!!'
+
+#----------------- Entrada al código -----------------#
+numeroOculto = 7
+print(MENSAJE_SALUDO)
+numeroIngresado = int(input(PREGUNTA_NUMERO))
+while(numeroOculto != numeroIngresado) :
+    numeroIngresado = int()
