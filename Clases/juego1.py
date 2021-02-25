a72c17f9b9a9ca96e6ca2a7572e0fa2f540d357e
nicollebanos <nicolleb2804@gmail.com>
2021-02-25 07:44:08 -0500

    F

--- Clases/juego1.py
+++ Clases/juego1.py
@@ -10,14 +10,23 @@ PREGUNTA_NUMERO = '''
                     muchos exitos, ingresa tu número 
 '''
 PREGUNTA_FALLASTE = 'Ahhhhhhhh! fallaste :3, ingresa otro número  '
-MENSAJE_DESPEDIDA = 'Felicidades, ganaste!!!'
+MENSAJE_GANASTE = 'Felicidades, ganaste!!!'
+MENSAJE_PERDISTE = 'Perdiste, vuelve a intentarlo!!!'
 
 #----------------- Entrada al código -----------------#
 print('------------ Inicio juego ---------------------')
 numeroOculto = 7
+vidas = 3
 print(MENSAJE_SALUDO)
 numeroIngresado = int(input(PREGUNTA_NUMERO))
-while(numeroOculto != numeroIngresado) :
+if(numeroIngresado != numeroOculto):
+    vidas -=1
+while(numeroOculto != numeroIngresado and vidas > 0) :
+    vidas -=1
     numeroIngresado = int(input(PREGUNTA_FALLASTE))
 
-print(MENSAJE_DESPEDIDA)
+if(vidas > 0):
+    print(MENSAJE_GANASTE)
+    print(vidas)
+else:
+    print(MENSAJE_PERDISTE)
