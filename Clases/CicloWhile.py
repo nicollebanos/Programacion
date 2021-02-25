6141939f036e08e8efcc25cebb8ef6160561aa9d
nicollebanos <nicolleb2804@gmail.com>
2021-02-25 07:02:37 -0500

    TRABAJO CLASE

---
+++ Clases/CicloWhile.py
@@ -0,0 +1,6 @@
+
+#-------- Mensajes ----------#
+MENSAJE_BIENVENIDA = "Bienvenid@!!!!! te apoyaré ahorrando"
+PREGUNTA_VALOR_PC = "Cuanto vale el pc que deseas?: "
+PREGUNTA_CUANDO_TIENES = "Cuanto llevas ahorrado?: "
+MENSAJE_AHORRO = "LLEVAS AHORRADO: "
