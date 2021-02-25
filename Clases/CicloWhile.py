35156ad65e302a8cdbb41e1cd8ae4d864f43efc6
nicollebanos <nicolleb2804@gmail.com>
2021-02-25 07:10:39 -0500

    trabajo

--- Clases/CicloWhile.py
+++ Clases/CicloWhile.py
@@ -6,10 +6,11 @@ PREGUNTA_CUANDO_TIENES = "Cuanto llevas ahorrado?: "
 MENSAJE_AHORRO = "LLEVAS AHORRADO: "
 
 #------ Entrada --------#
+print("---------------- Bienvenida -----------------")
 print(MENSAJE_BIENVENIDA)
 valor = float(input(PREGUNTA_VALOR_PC))
 ahorrado = float(input(PREGUNTA_CUANDO_TIENES))
 
-while(valor >= ahorrado):
+while(valor > ahorrado):
     print(MENSAJE_AHORRO, ahorrado, "Te falta", valor - ahorrado)
-    ahorrado = +1
+    ahorrado = +1000
