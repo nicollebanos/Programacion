741ad818d093db51aa2453252e86200688c326e1
nicollebanos <nicolleb2804@gmail.com>
2021-02-25 07:04:20 -0500

    trabajo

--- Clases/CicloWhile.py
+++ Clases/CicloWhile.py
@@ -4,3 +4,10 @@ MENSAJE_BIENVENIDA = "Bienvenid@!!!!! te apoyaré ahorrando"
 PREGUNTA_VALOR_PC = "Cuanto vale el pc que deseas?: "
 PREGUNTA_CUANDO_TIENES = "Cuanto llevas ahorrado?: "
 MENSAJE_AHORRO = "LLEVAS AHORRADO: "
+
+#------ Entrada --------#
+print(MENSAJE_BIENVENIDA)
+valor = float(input(PREGUNTA_VALOR_PC))
+ahorrado = float(input(PREGUNTA_CUANDO_TIENES))
+
+while()
