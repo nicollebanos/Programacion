d94a334c3e38cbe8556301af06799f6802a82126
nicollebanos <nicolleb2804@gmail.com>
2021-03-02 07:07:32 -0500

    f

---
+++ Clases/juego2.py
@@ -0,0 +1,18 @@
+
+
+#----------- Entradas ----------#
+MENSAJE_SALUDO =  '''Bienvenido 
+                    a este programa,
+                    juguemos!!'''
+PREGUNTA_NUMERO = '''
+                    En este juego debes asertar un número entero
+                    que va desde el 1-10, la idea es que
+                    lo puedes intentar las veces que 
+                    quieras...
+                    muchos exitos, ingresa tu número 
+'''
+PREGUNTA_FALLASTE = 'Ahhhhhhhh! fallaste :3, ingresa otro número  '
+MENSAJE_GANASTE = 'Felicidades, ganaste!!!'
+MENSAJE_PERDISTE = 'Perdiste, vuelve a intentarlo!!!'
+
+#----------------- Entrada al código -----------------#
