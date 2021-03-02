e3ab82c0e68fdea6abfeccaf237a9ed964849526
nicollebanos <nicolleb2804@gmail.com>
2021-03-02 07:29:57 -0500

    f

--- Clases/juego2.py
+++ Clases/juego2.py
@@ -14,14 +14,30 @@ PREGUNTA_NUMERO = '''
 PREGUNTA_FALLASTE = 'Ahhhhhhhh! fallaste :3, ingresa otro número  '
 MENSAJE_GANASTE = 'Felicidades, ganaste!!!'
 MENSAJE_PERDISTE = 'Perdiste, vuelve a intentarlo!!!'
+PREGUNTA_DIFICULTAD = '''
+            1- Fácil
+            2- Moderado
+            3- Difícil
+'''
 
 #----------------- Entrada al código -----------------#
 numeroOculto = random.randint(1,10)
-vidas = 3
+vidas = ''
+dificultad = int(input(PREGUNTA_DIFICULTAD))
+if(dificultad == 1):
+    vidas = 5
+elif(dificultad == 2):
+    vidas = 3
+else:
+    vidas = 1
+
 numeroIngresado = int (input(PREGUNTA_NUMERO))
 while(numeroIngresado != numeroOculto and vidas>1):
     vidas -=1
-    print(vidas)
-    print(numeroOculto)
+    print(F'te quedan {vidas} vidas')
     numeroIngresado =int(input(PREGUNTA_FALLASTE))
     
+if(vidas >= 0 and numeroIngresado == numeroOculto):
+    print(MENSAJE_GANASTE)
+else:
+    print(MENSAJE_PERDISTE, 'El número era el ', numeroOculto)
