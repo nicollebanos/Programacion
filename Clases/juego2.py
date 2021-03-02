1fb919908317e857241bcdc9dfaf6880696aac93
nicollebanos <nicolleb2804@gmail.com>
2021-03-02 07:20:56 -0500

    f

--- Clases/juego2.py
+++ Clases/juego2.py
@@ -7,8 +7,8 @@ MENSAJE_SALUDO =  '''Bienvenido
 PREGUNTA_NUMERO = '''
                     En este juego debes asertar un número entero
                     que va desde el 1-10, la idea es que
-                    lo puedes intentar las veces que 
-                    quieras...
+                    lo puedes intentar antes de que se te 
+                    acaben las vidas... 
                     muchos exitos, ingresa tu número 
 '''
 PREGUNTA_FALLASTE = 'Ahhhhhhhh! fallaste :3, ingresa otro número  '
