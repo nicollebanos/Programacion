9d1c5491f43c87410456c99452053ad681b44ede
nicollebanos <nicolleb2804@gmail.com>
2021-03-04 06:51:28 -0500

    for

---
+++ Clases/ciclosfor.py
@@ -0,0 +1,15 @@
+
+for iteracion in range(10):
+    print(iteracion)
+
+print("--------------------------")
+for iteracion in range(10):
+    print(iteracion + 1)
+
+print("--------------------------")
+for iteracion in range (1,11):
+    print(iteracion)
+
+print("--------------------------")
+for iteracion in range (1,11,2):
+    print(iteracion)
