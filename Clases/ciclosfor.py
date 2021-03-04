4e7b8b2071490370acdfe72c88f3fa2ffab21dec
nicollebanos <nicolleb2804@gmail.com>
2021-03-04 07:02:52 -0500

    f

--- Clases/ciclosfor.py
+++ Clases/ciclosfor.py
@@ -24,3 +24,15 @@ print("--------------------------")
 for iteracion in range(1,11):
     if(iteracion%2 == 0):
         print(iteracion)
+
+print("--------------------------")
+for iteracion in range(1,11):
+    if(iteracion%2 != 0):
+        print(iteracion)
+
+print("--------------------------")
+for iteracion in range (1,11):
+    if(iteracion % 2 == 0):
+        print(iteracion, '--> Es un número par')
+    else:
+        print(iteracion, '--> Es un número impar')
