46e1867ef374ef23a7425483bee40ab931c825c8
nicollebanos <nicolleb2804@gmail.com>
2021-03-04 07:14:07 -0500

    f

--- Clases/ciclosfor.py
+++ Clases/ciclosfor.py
@@ -40,5 +40,32 @@ for iteracion in range (1,11):
 print("--------------------------")
 rango = int(input('Ingrese el rango máximo: '))
 
-for iteracion in range (1,rango+1):
-    print(iteracion)
+opcion = int(input('''
+    1- Ver solo impares
+    2- Ver solo pares
+    3- Ver las dos clasificaciones
+    4- Cualquier número para mostrar nada
+
+'''))
+
+print("--------------------------")
+if(opcion == 1):
+    for iteracion in range (1,rango+1):
+        if(iteracion % 2 != 0):
+            print(iteracion)
+elif (opcion == 2):
+    for iteracion in range (1,rango+1):
+        if(iteracion % 2 == 0):
+            print(iteracion)
+elif(opcion == 3):
+    for iteracion in range (1,rango+1):
+        if(iteracion % 2 == 0):
+            print(iteracion, '--> Es un número par')
+        else:
+            print(iteracion, '--> Es un número impar')
+else:
+    print('Mostrar nada')
+
+
+
+
