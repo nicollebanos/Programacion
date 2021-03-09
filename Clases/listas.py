6c8cfbf07761017fdae5ec6572ce86944c3ea8ca
nicollebanos <nicolleb2804@gmail.com>
2021-03-09 07:33:14 -0500

    f

--- Clases/listas.py
+++ Clases/listas.py
@@ -6,7 +6,7 @@ nombres.append('Mauricio')
 print(nombres)
 print(nombres[2])
 
-edades = [18, 19, 20, 17,32]
+edades = [18, 19, 20, 17,32, 12, 15, 13]
 estaturas = [1.62, 1.80, 1,67, 1.89]
 # al último
 print(edades[-2])
@@ -56,3 +56,29 @@ for indice in range (largoListaEdades):
 largoListaEdades = len(nombres)
 for indice in range (largoListaNombres):
     print(nombres[indice])
+
+#posiciones lista par
+posicionesPares = []
+largoListaEdades = len(edades)
+for posicion in range (largoListaEdades):
+    if(edades[posicion]%2 == 0):
+        posicionesPares.append(posicion)
+
+print(edades)
+print(posicionesPares)
+
+#solo cuando nos interese mostrar la lista
+for edad in edades:
+    print(edad)
+for nombre in nombres:
+    print(nombre)
+    print(posicion)
+    posicion+=1
+
+posicion = 0
+posicionesPares = []
+for edad in edades:
+    if(edad%2 == 0):
+        posicionesPares.append(posicion)
+    posicion+=1
+print(posicionesPares)
