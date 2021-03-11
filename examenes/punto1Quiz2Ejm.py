0b0a84d5d4d9430231fc29adca747655da5d8ede
nicollebanos <nicolleb2804@gmail.com>
2021-03-11 07:18:13 -0500

    f

--- examenes/punto1Quiz2Ejm.py
+++ examenes/punto1Quiz2Ejm.py
@@ -8,5 +8,26 @@ PREGUNTA_MONEDA = '''
 
 listaEuros = []
 for elemento in listaPesos:
-    listaEuros.append(elemento*0.00023)
+    conversor = round(elemento*0.00023,2)
+    listaEuros.append(conversor)
+
+listaDolares = []
+for elemento in listaPesos:
+    conversor = round(elemento*0.00028,2)
+    listaDolares.append(conversor)
+
+opcionMoneda = input(PREGUNTA_MONEDA)
+if(opcionMoneda == 'C'):
+    print('Mostrando lista original')
+    print(listaPesos)
+elif(opcionMoneda == 'D')
+    print('Mostrar lista en dolares')
+    print(listaDolares)
+elif(opcionMoneda == 'E')
+    print('Mostrar lista en Euros')
+    print(listaEuros)
+else:
+    print('Valor ingresado no valido')
+
 print(listaEuros)
+print(listaDolares)
