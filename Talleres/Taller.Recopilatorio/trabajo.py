c91315f2f05505266d3cf093861c1755fa2ed1cd
nicollebanos <nicolleb2804@gmail.com>
2021-03-14 15:45:39 -0500

    f

--- Talleres/Taller.Recopilatorio/trabajo.py
+++ Talleres/Taller.Recopilatorio/trabajo.py
@@ -1,4 +1,14 @@
 
+#----Preguntas
+preguntaNumero = '''Ingrese alguna de estas opciones
+    1.Hacer conversión de dolares a pesos colombianos o Euros
+    2.Agrege un valor a la lista de dolares
+    3.Mostrar si es bajo, medio alto o elevado ingreso
+    4. Mostrar ingreso más alto, más bajo y promedio
+    5.Salir
+'''
+
+#-----------------------Punto 1----------------------#
 listaDolares = [20000,30000,4000,2500,1000,7600]
 preguntaMoneda = '''
     C- Mostrar en pesos colombiano
@@ -33,5 +43,8 @@ while(opcionMoneda != 'C', 'D', 'E'):
     else:
         print(mensajeErrorEntrada)
     opcionMoneda = input(preguntaMoneda)
+
+
+#--------------------- Punto 2 -----------------------#
 
 
