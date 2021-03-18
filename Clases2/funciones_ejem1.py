72c2be9d7887efd843c3fd4e6ed0ca275d3bf577
nicollebanos <nicolleb2804@gmail.com>
2021-03-18 07:21:38 -0500

    f

--- Clases2/funciones_ejem1.py
+++ Clases2/funciones_ejem1.py
@@ -5,13 +5,14 @@ print(guardar)
 guardar = round(14.2534897,2)
 print(guardar)
 
-def linedesing(cantidad, simbolo):
+def linedesing(cantidad=10, simbolo='$'):
     print(simbolo*cantidad)
     return None
 
 linedesing(30,'#')
 linedesing(10,'*')
 linedesing(40,'!')
+linedesing() #--- Por defecto ---#
 
 #------------- Muestre la lista -------------#
 def mostrarLista(inLista):
@@ -24,3 +25,13 @@ linedesing(30,':v')
 mostrarLista(lista)
 linedesing(30,'.-.')
 mostrarLista(lista2)
+
+
+#------------- sumar dos númerod -------------#
+def sumar (a = 0,b = 0):
+    suma = a + b
+    return suma
+linedesing(10,':v')
+resultado = sumar()
+print(sumar(12,14))
+round(12.23456,2)
