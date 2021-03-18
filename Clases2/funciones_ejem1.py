4c04ccb6d1d8d29c60b1b6aa931dbf12c2f1908f
nicollebanos <nicolleb2804@gmail.com>
2021-03-18 07:01:19 -0500

    f

---
+++ Clases2/funciones_ejem1.py
@@ -0,0 +1,14 @@
+
+guardar = print('Hola')
+print(guardar)
+
+guardar = round(14.2534897,2)
+print(guardar)
+
+def linedesing(cantidad, simbolo):
+    print(simbolo*cantidad)
+    return None
+
+linedesing(30,'#')
+linedesing(10,'*')
+linedesing(40,'!')
