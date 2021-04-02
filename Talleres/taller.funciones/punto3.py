512c33548d196394fa21d88318ecccc1d9d1e09b
nicollebanos <nicolleb2804@gmail.com>
2021-04-02 14:00:00 -0500

    f

---
+++ Talleres/taller.funciones/punto3.py
@@ -0,0 +1,10 @@
+
+#------------------- Salude n veces -----------------------#
+
+def saludar (cantidad = 0):
+  print ('Buenos días estrellitas'* cantidad)
+saludar (1)
+saludar (1)
+saludar (1)
+saludar (1)
+saludar (1)
