5ec78e38965be1fe8f7f7ae36ebd8747e6cf04e3
nicollebanos <nicolleb2804@gmail.com>
2021-04-02 14:31:36 -0500

    f

---
+++ Talleres/taller.funciones/punto7.py
@@ -0,0 +1,9 @@
+
+#---------- Crea un función que sirva para despedirte del que esta ejecutando el código -----------#
+
+PREGUNTA_NOMBRE = 'Como te llamas: '
+nombre = input(PREGUNTA_NOMBRE) 
+
+def chao():
+  print ('Gracias por confiar en nosotros, un gusto conocerte', nombre )
+chao()
