247e1e2a18e7eebb46306aa14cb6a9c12d52e201
nicollebanos <nicolleb2804@gmail.com>
2021-04-02 13:45:06 -0500

    f

---
+++ Talleres/taller.funciones/punto1.py
@@ -0,0 +1,10 @@
+
+#-------------Dada una lista muéstrela en pantalla elemento a elemento---------------#
+def mostrarLista (lista):
+  for elemento in lista :
+    print (elemento)
+
+nombres = ['Nicolle', 'Gabriela', 'Lobo', 'Ignacio', 'Hugo', 'Clarissa']
+edades = [19, 16, 19, 20, 20, 22]
+mostrarLista(edades)
+mostrarLista(nombres)
