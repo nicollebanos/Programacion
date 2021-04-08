09d1dfeee14282e4ac27b42b6e313a962794b55c
nicollebanos <nicolleb2804@gmail.com>
2021-04-08 06:52:57 -0500

    LISTA

--- examenes/Parcial/punto2.py
+++ examenes/Parcial/punto2.py
@@ -2,3 +2,20 @@
 # Crear una función que dada tres listas del mismo 
 # tamaño las muestre en pantalla
 
+
+def mostrarLista (lista):
+  for elemento in lista :
+    print (elemento)
+
+nombres = ['Gabito', 'Nacho', 'Lobito', 'Hugo', 'Nicky']
+edades = [17, 20, 19, 21, 18]
+estatura = [1.57, 1.60, 1.64, 1.70, 1.71]
+
+print('#----- Lista nombres -----#')
+mostrarLista(nombres)
+
+print('#----- Lista edades -----#')
+mostrarLista(edades)
+
+print('#----- Lista estaturas -----#')
+mostrarLista(estatura)
