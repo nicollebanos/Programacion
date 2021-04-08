8a5b8cc02a2616848bedc7536a399690c78f946b
nicollebanos <nicolleb2804@gmail.com>
2021-04-08 07:28:53 -0500

    lloro

---
+++ examenes/Parcial/punto5.py
@@ -0,0 +1,10 @@
+# se sabe que la sucesión de 
+# Fibonacci sigue el siguiente patrón 
+# 0,1,1,2,3,5,8,13,21,34,55,89,144, …. Se le pide como 
+# ingeniero biomédico, que dado un número n de la 
+# sucesión muestre en pantalla su valor. Por ejemplo, si el 
+# usuario digita 3 se mostrará 1 y si el usuario digita 5 
+# mostrará 3 si ingresa 100 debe mostrar el número que le 
+# corresponda en la sucesión.
+
+listaFibonacci = [0,1,1,2,3,5,8,13,21,34,55,89,144]
