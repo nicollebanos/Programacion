e73647d6910dd3614d0dff9d2df9fbf112728d43
nicollebanos <nicolleb2804@gmail.com>
2021-04-14 20:09:58 -0500

    f

--- Clases2/clasesyobjetos/tarea1.py
+++ Clases2/clasesyobjetos/tarea1.py
@@ -26,12 +26,13 @@ class Perro ():
 
     def dormir (self):
         print(f'''Podria decir que me considero un {self.raza} muy dormilon.
-        Duermo toda la noche y hago siestas el medio día.
-''')
+    Duermo toda la noche y hago siestas al medio día.
+    ''')
 
     def jugar (self):
-        print(f''' Vivo en una/la {self.habitat}, juego al máximo día y 
-        noche hasta caer cansado, se pasa un rato agradable.
+        print(f''' Ademas de eso, vivo en una/la {self.habitat} difrutando y 
+    jugando al máximo día y noche hasta caer cansado, 
+    se pasa un rato agradable.
     ''')
 
     def mostrarAtributos (self):
@@ -39,20 +40,23 @@ class Perro ():
     Me encuentro viviendo en una/la {self.habitat} y soy de 
     raza {self.raza} lo que me hace atractivo 
     para las personas cuando me ven.
-''')
+    ''')
 
 perro1 = Perro('Pancho', 'Bugdog Inglés', 'casa', 'macho')
 perro2 = Perro('Milu', 'Chihuahua', 'casa', 'hembra')
 perro3 = Perro('Rocky', 'Golden retriever', 'calle', 'macho')
 
+print('--------------- Perro 1 ---------------')
 perro1.mostrarAtributos()
 perro1.dormir()
 perro1.jugar()
 
+print('--------------- Perro 2 ---------------')
 perro2.mostrarAtributos()
 perro2.dormir()
 perro2.jugar()
 
+print('--------------- Perro 3 ---------------')
 perro3.mostrarAtributos()
 perro3.dormir()
 perro3.jugar()
