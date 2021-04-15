a20238bb50a9c3df9f8cf072c1243d947ec7d869
nicollebanos <nicolleb2804@gmail.com>
2021-04-15 07:34:55 -0500

    f

--- Clases2/clasesyobjetos/utilizandoClases.py
+++ Clases2/clasesyobjetos/utilizandoClases.py
@@ -1,8 +1,8 @@
 
 import introduccion as moldes
 
-biomedicio = moldes.Biomedico('Aleja',20,1.63,'Biomédicina')
-biomedicio.mostrarAtributos()
-biomedicio.recorrerDistancia(27)
-biomedicio.solucionarProblemas('Alta demanda en equipos médicos extranjeros')
-biomedicio.crearAlgoritmo('Detector de picos ECG')
+biomedico = moldes.Biomedico('Aleja',20,1.63,'Biomédicina')
+biomedico.mostrarAtributos()
+biomedico.recorrerDistancia(27)
+biomedico.solucionarProblemas('Alta demanda en equipos médicos extranjeros')
+biomedico.crearAlgoritmo('Detector de picos ECG') 
