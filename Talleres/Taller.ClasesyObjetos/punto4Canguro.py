9b5a626531b774832a86b9b668b4ee9992caaefa
nicollebanos <nicolleb2804@gmail.com>
2021-04-20 12:31:14 -0500

    error

--- Talleres/Taller.ClasesyObjetos/punto4Canguro.py
+++ Talleres/Taller.ClasesyObjetos/punto4Canguro.py
@@ -1,7 +1,7 @@
 # Cree una clase Canguro que tenga los atributos edad, id, nombre. 
 # Cree una función que, dada una cantidad de saltos, 
 # muestre en pantalla uno a uno los saltos del canguro. 
-# Ejemplo : cangurp.saltos (5) →retorna lo siguiente :
+# Ejemplo : canguro.saltos (5) →retorna lo siguiente :
 # El canguro ha dado 1 salto
 # El canguro ha dado 2 salto
 # El canguro ha dado 3 salto
@@ -27,6 +27,13 @@ class Canguro ():
         ID  número {self.id}
         ''')
 
+    def saltosCanguro (self, saltosEntrada):
+        self.saltos = saltosEntrada
+        for iteracion in range():
+            print('El canguroha dado', iteracion, 'salto')
+
+
 canguro = Canguro(10, 206, 'Camilo' )
 
 canguro.mostrarAtributos()
+canguro.saltosCanguro(5)
