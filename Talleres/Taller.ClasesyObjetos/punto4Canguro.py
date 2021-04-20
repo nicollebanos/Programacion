7bcfa8263151483be6e69b1d32b3aea67b85964d
nicollebanos <nicolleb2804@gmail.com>
2021-04-20 11:43:46 -0500

    canguro

---
+++ Talleres/Taller.ClasesyObjetos/punto4Canguro.py
@@ -0,0 +1,8 @@
+# Cree una clase Canguro que tenga los atributos edad, id, nombre. Cree una función que,
+# dada una cantidad de saltos, muestre en pantalla uno a uno los saltos del canguro. 
+# Ejemplo : cangurp.saltos (5) →retorna lo siguiente :
+# El canguro ha dado 1 salto
+# El canguro ha dado 2 salto
+# El canguro ha dado 3 salto
+# El canguro ha dado 4 salto
+# El canguro ha dado 5 salto
