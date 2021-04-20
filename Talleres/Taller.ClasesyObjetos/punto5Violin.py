1c7e8b9ee06f204d05eb85a2b87919c227dcab6f
nicollebanos <nicolleb2804@gmail.com>
2021-04-20 13:05:42 -0500

    violin

---
+++ Talleres/Taller.ClasesyObjetos/punto5Violin.py
@@ -0,0 +1,6 @@
+
+# Piense en su instrumento musical favorito y cree una clase 
+# asociada al mismo, defina los 
+# atributos y cree una función que permita dada una canción 
+# interpretarla en el mismo
+
