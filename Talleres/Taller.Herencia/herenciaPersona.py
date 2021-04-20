325c88ecdd2195353b2c90577d10e21277bf0730
nicollebanos <nicolleb2804@gmail.com>
2021-04-20 14:40:48 -0500

    persona

---
+++ Talleres/Taller.Herencia/herenciaPersona.py
@@ -0,0 +1,51 @@
+
+#------------------- PUNTO 1 ------------------#
+#Cree la clase Persona con id, nombre, edad y cree 
+#la función hablar la cual dado mensaje 
+#se muestra en pantalla y cree la clase caminar 
+#que dado una cantidad de pasos muestra en 
+#pantalla cuanto ha caminado
+
+class Persona ():
+    '''
+    nombreEntrada: Hace referencia al nombre
+    edadEntrada: Hace referencia a la edad
+    idEntrada: Hace referencia al ID
+    Acción:
+    *Hablar(accion): 
+        Comentario
+    *Caminar(accion):
+        Pasos dados
+    '''
+
+    def __init__(self, nombreEntrada, edadEntrada, idEntrada):
+        self.nombre = nombreEntrada
+        self.edad = edadEntrada
+        self.id = idEntrada
+
+
+    def mostrarAtributos (self):
+        print(f''' Soy {self.nombre}, tengo {self.edad} de
+        edad y me identifico con el número de cedula
+        {self.id} 
+        ''')
+
+    def hablar(self,mensaje):
+        print(f'Tengo un mensaje que decir...', mensaje)
+
+
+persona1 = Persona('Camilo', 18, 1003293645)
+persona2 = Persona('Angelica', 21, 1004283456)
+persona3 = Persona('Marlon', 28, 1002384512)
+
+print('--------------- persona 1 ---------------')
+persona1.mostrarAtributos()
+persona1.hablar('Hoy me siento muy feliz')
+
+print('--------------- persona 2 ---------------')
+persona2.mostrarAtributos()
+persona2.hablar('Hoy no estoy muy bien, pero sigo de pie')
+
+print('--------------- persona 3 ---------------')
+persona3.mostrarAtributos()
+persona3.hablar('Hoy es un día más, sere feliz')
