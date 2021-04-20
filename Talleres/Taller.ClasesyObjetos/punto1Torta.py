f3a4a8df78755107bdc26c652dfdc27b5b2715cb
nicollebanos <nicolleb2804@gmail.com>
2021-04-20 06:56:03 -0500

    f

--- Talleres/Taller.ClasesyObjetos/punto1Torta.py
+++ Talleres/Taller.ClasesyObjetos/punto1Torta.py
@@ -1,3 +1,18 @@
 
 # Cree la clase Torta con atributos forma, sabor, altura, también tendrá una función donde 
 # muestre todos sus atributos.
+
+class Torta ():
+    '''Una torta se usa tradicionalmnete para celebrar algo especial, 
+    por lo que presenta los siguientes atributos:
+
+    formaEntrada: Hace referencia a la forma de la torta
+    saborEntrada: Hace referencia al sabor de la torta
+    alturaEntrada: Hace referencia a la altura de la torta
+    '''
+
+    def __init__(self,formaEntrada, saborEntrada, alturaEntrada):
+        print("Holis, soy una perro muy amoroso")
+        self.forma = formaEntrada
+        self.sabor = saborEntrada
+        self.altura = alturaEntrada
