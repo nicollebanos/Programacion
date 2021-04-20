bacc4560a95c6ad8708eaef4c3b3e802fc65f95f
nicollebanos <nicolleb2804@gmail.com>
2021-04-20 11:38:05 -0500

    F

--- Talleres/Taller.ClasesyObjetos/punto3Nutricionista.py
+++ Talleres/Taller.ClasesyObjetos/punto3Nutricionista.py
@@ -20,23 +20,30 @@ class Nutricionista ():
 
 
     def mostrarAtributos (self):
-        print(f''' Buenas tardes soy la nutricionista {self.nombre},
+        print(f''' Buenas tardes soy el nutricionista {self.nombre},
         tengo {self.edad} años y curse mis estudios en 
         la univdersidad {self.universidad}.
     ''')
 
-    def indiceMasaCorporal (self, pesoEntrada, alturaEntrada):
+    def masaCorporal (self, pesoEntrada, alturaEntrada):
         self.peso = pesoEntrada
         self.altura = alturaEntrada
-        print(f'''
+        IMC : self.peso / (self.altura**2)
+        print(f''' Como tu nutriologo, vericando los datos
+        de {self.peso} kg  de peso y {self.altura} m de altura 
+        dando como resultado como IMC {IMC}
         ''')
 
-nutricionista = Nutricionista(19, 'Juan', 'CES')
 
 
-#----------------- Print ----------------------#
+nutricionista = Nutricionista(30, 'Juan', 'CES')
+
+
+
 print('--------------- nutricionista ---------------')
 nutricionista.mostrarAtributos()
+nutricionista.masaCorporal(57, 1.58)
+
 
 
 
