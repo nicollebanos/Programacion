6f47eecc418ca7476da372970a24bc0641a856ab
nicollebanos <nicolleb2804@gmail.com>
2021-04-19 20:22:39 -0500

    f

--- Clases2/clasesyobjetos/introduccion.py
+++ Clases2/clasesyobjetos/introduccion.py
@@ -1,10 +1,11 @@
 
-class Humano():
+class Humano ():
     '''
         Esta es la clase Humano exige atributos
         nombreEntrada: Hace referencia al nombre del usuario
         edadEntrada: Hace referencia al edad del usuario
         estaturaEntrada: Hace referencia al estatura del usuario
+        
         Tiene las siguientes acciones:
         *hablar(mensaje):
             dado un mensaje lo muestra en pantalla
@@ -12,90 +13,84 @@ class Humano():
         *mostrarAtributos()
             muestra los atributos del usuario
     '''
-    def __init__(self, nombreEntrada, edadEntrada,estaturaEntrada):
-        print('Hola soy un humano nuevo')
+    def __init__(self,nombreEntrada,edadEntrada,estaturaEntrada):
+        print("Hola, soy un humano nuevo")
         self.edad = edadEntrada
-        self.raza = 'Humano'
+        self.raza = "Humano"
         self.nombre = nombreEntrada
         self.estatura = estaturaEntrada
         self.dinero = 0
-    
+
     def hablar(self,mensaje):
-        print(f'Hola soy {self.nombre} tengo un mensaje que decir ...', mensaje)
+        print(f"Hola soy {self.nombre} tengo un mensaje que decir...", mensaje)
     
-    def mostrarAtributos(self):
+    def mostrarAtributos (self):
         print(f'''Mi nombre es {self.nombre}
                     Mi estatura es {self.estatura} metros
                     Mi edad es {self.edad} años
                     Tengo ahorrado {self.dinero} pesos
         ''')
+
     def recorrerDistancia(self,distanciaMetros):
         for i in range (distanciaMetros):
-            print(f'Hola soy {self.nombre} y he recorrido {i+1} metros')
+            print(f"Hola soy {self.nombre} y he recorrido {i + 1} metros")
 
     def ahorraDinero(self):
-        preguntaIngresarMontos = 'Ingrese S--> para continuar añadiendo montos y N--> para finalizar : '
-        preguntaMonto = 'Cuanto vas a ingresar?: '
-        ingresarMontos =input(preguntaIngresarMontos)
-        while(ingresarMontos != 'N'):
+        preguntaIngresarMonto = "Ingrese S para continuar añadiendo montos y N para finalizar: "
+        preguntaMonto = "¿Cuánto vas a ingresar?: "
+        ingresarMontos = input(preguntaIngresarMonto)
+        while(ingresarMontos != "N"):
             monto = float(input(preguntaMonto))
             self.dinero = self.dinero + monto
-            print(f'Soy {self.nombre} y tengo {self.dinero} pesos')
-            ingresarMontos =input(preguntaIngresarMontos)
+            print(f"Soy {self.nombre} y tengo {self.dinero} pesos")
+            ingresarMontos = input(preguntaIngresarMonto)
         return self.dinero
 
+
 class Ingeniero(Humano):
-    def __init__(self,nombreEntrada, edadEntrada,estaturaEntrada,areaEntrada):
-        Humano.__init__(self, nombreEntrada,edadEntrada,estaturaEntrada)
+    def __init__(self,nombreEntrada,edadEntrada,estaturaEntrada,areaEntrada):
+        Humano.__init__(self,nombreEntrada,edadEntrada,estaturaEntrada)
         self.area = areaEntrada
 
-    def solucionarProblemas(self, problema):
-        print(f'Hola soy un ingeniero y me llamo {self.nombre} y procedo a solucionar el problema {problema}')
-class Programador (Humano):
-    def crearAlgoritmo(self, algoritmo):
-        print(f'Hola soy {self.nombre} y procedo a resolver el algoritmo {algoritmo}')
 
-class Biomedico(Ingeniero, Programador):
-    def mantenimientoEquiposMedicos(self, nombreEquipo ):
-        print (f'Hola soy {self.nombre} y procedo a arreglar el {nombreEquipo}')
-    
+    def solucionarProblemas(self,problema):
+        print(f"Hola soy un ingeniero/a y me llamo {self.nombre} y procedo a solucionar el problema {problema}")
+
+class Programador(Humano):
+    def crearAlgoritmo(self,algoritmo):
+        print(f"Hola soy {self.nombre} y procedo a resolver el algoritmo {algoritmo}")
+
+class Biomedico(Ingeniero,Programador):
+    def mantenimientoEquiposMedicos(self,nombreEquipo):
+        print(f"Hola soy {self.nombre} y procedo a arreglar el {nombreEquipo}")
 
 class Abogado(Humano):
     def levantarAccionDeTutela(self,nombreCliente):
-        print(f'Hola soy {self.nombre} y estoy representando a {nombreCliente}')
+        print(f"Hola soy {self.nombre} y estoy representando a {nombreCliente}")
 
+# humano1 = Humano('Levi',27,1.67)
+# humano2 = Humano('Armin',27,1.73)
 
+# humano1.hablar("Esta haciendo un día lindo")
+# humano2.hablar("Adiós amigo")
+# print(humano1.nombre)
+# print(humano2.nombre)
+# print(humano2.edad)
+# humano1.mostrarAtributos()
+# humano1.recorrerDistancia(25)
+# humano2.mostrarAtributos()
+# totalAhorrado = humano2.ahorraDinero()
+# humano2.mostrarAtributos()
 
+# biomedico1 = Biomedico("Isabel",19,1.58,"Biomédicina")
+# biomedico1.recorrerDistancia(25)
+# biomedico1.mostrarAtributos()
+# biomedico1.mantenimientoEquiposMedicos("Electrocardiograma")
 
+# abogado1 = Abogado("Jason",37,1.79)
+# abogado1.mostrarAtributos
+# abogado1.levantarAccionDeTutela(biomedico1.nombre)
 
-
-
-
-
-
-
-
-humano1 = Humano('Daniel',27,1.67)
-humano2 = Humano('Mafer',27,1.60)
-
-humano1.hablar('Espero que esten muy bien')
-humano2.hablar('chao')
-print(humano1.nombre)
-print(humano2.nombre)
-print(humano2.edad)
-humano1.mostrarAtributos()
-humano1.recorrerDistancia(25)
-humano2.mostrarAtributos()
-totalAhorrado = humano2.ahorraDinero()
-humano2.mostrarAtributos()
-biomedico1 = Biomedico('Karla',20,1.63,'Biomédicina')
-biomedico1.recorrerDistancia(25)
-biomedico1.mostrarAtributos()
-biomedico1.mantenimientoEquiposMedicos('Electrocardiograma')
-
-abogado1 = Abogado('Stiven',34,1.94)
-abogado1.mostrarAtributos()
-abogado1.levantarAccionDeTutela(biomedico1.nombre)
-biomedico1.crearAlgoritmo('Fibonacci')
-biomedico1.solucionarProblemas('Ocupación alta de UCIs')
-print(biomedico1.area)
+# biomedico1.crearAlgoritmo("Fibonacci")
+# biomedico1.solucionarProblemas("Ocupación altas de UCIs")
+# print(biomedico1.area)
--- Clases2/clasesyobjetos/utilizandoClases.py
+++ Clases2/clasesyobjetos/utilizandoClases.py
@@ -1,8 +1,8 @@
 
-import introduccion as molde
+import introduccion as moldes
 
-biomedico = molde.Biomedico('Aleja',20,1.63,'Biomédicina')
+biomedico = moldes.Biomedico("Sara",18,1.65,"Biomédicina")
 biomedico.mostrarAtributos()
-biomedico.recorrerDistancia(27)
-biomedico.solucionarProblemas('Alta demanda en equipos médicos extranjeros')
-biomedico.crearAlgoritmo('Detector de picos ECG') 
+biomedico.recorrerDistancia(16)
+biomedico.solucionarProblemas("Alta demanda en equipos médicos extranjeros")
+biomedico.crearAlgoritmo("Detector de picos ECG")
