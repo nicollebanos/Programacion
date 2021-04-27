f900b1181655dc4083d0c0843b54271fe1ececbf
nicollebanos <nicolleb2804@gmail.com>
2021-04-27 07:10:26 -0500

    f

---
+++ Clases3/grafico/graficoBarras.py
@@ -0,0 +1,11 @@
+#Aqui explicaremos como hacer un grafico de barras
+
+import matplotlib.pyplot as plt 
+lenguaje = ['py', 'java', 'dart', 'ts', 'elixir']
+encuesta = [50,10,20,10,10]
+plt.bar(lenguaje, encuesta)
+###############################
+
+
+##############################
+plt.show()
