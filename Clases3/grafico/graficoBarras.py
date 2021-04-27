feda6e051c022cbcdf71e22af8f7309afbecb5ea
nicollebanos <nicolleb2804@gmail.com>
2021-04-27 07:13:37 -0500

    grafico

--- Clases3/grafico/graficoBarras.py
+++ Clases3/grafico/graficoBarras.py
@@ -5,7 +5,10 @@ lenguaje = ['py', 'java', 'dart', 'ts', 'elixir']
 encuesta = [50,10,20,10,10]
 plt.bar(lenguaje, encuesta)
 ###############################
-
+plt.title('Lenguajes más usados')
+plt.xlabel('Lenguaje de programación')
+plt.ylabel('% de uso de lenguaje')
+plt.savefig('graficoLenguaje.png')
 
 ##############################
 plt.show()
