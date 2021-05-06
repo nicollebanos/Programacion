7d266252bbf249d94ebe5d4198271a674a36ae1d
nicollebanos <nicolleb2804@gmail.com>
2021-05-06 07:55:01 -0500

    f

---
+++ Talleres/Taller.Graficos/PPG1.py
@@ -0,0 +1,16 @@
+
+import pandas as pd
+import matplotlib.pyplot as plt
+ppgData = pd.read_csv('ppg.csv',encoding='UTF-8',header=0,delimiter=';').to_dict()
+print(ppgData.keys())
+muestras = list(ppgData['muestra'].values())
+print(muestras)
+voltaje = list(ppgData['valor'].values())
+print(voltaje[-10:])
+plt.plot(muestras, voltaje)
+plt.title('Fotoplestimografia')
+plt.xlabel('Tiempo')
+plt.ylabel('voltaje(mV)')
+plt.show()
+plt.savefig('GraficoFotplestimografia.png')
+
