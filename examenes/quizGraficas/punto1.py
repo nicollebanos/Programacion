672f28830034bbdcd762d22966034bdff12750e2
nicollebanos <nicolleb2804@gmail.com>
2021-05-11 07:30:28 -0500

    f

--- examenes/quizGraficas/punto1.py
+++ examenes/quizGraficas/punto1.py
@@ -4,12 +4,15 @@
 # gráfico y a sus ejes también recuerde guardar el resultado en 
 # un archivo png)
 
-PREGUNTA_SNACKS = 'Ingrese sus 5 snacks favoritos'
-PREGUNTA_PRECIOS = 'Ingrese los precios'
-
-
 import matplotlib.pyplot as plt
+snacksFav = ['Doritos', 'DeTodito', 'Trululu', 'jugo Hit', 'Choclitos']
+precio = [3000, 1300, 2500, 3500, 2300]
+plt.bar(snacksFav, precio, width= 0.5, color = 'c')
 
-snacksFav = input(PREGUNTA_SNACKS)
-precios = float(input(PREGUNTA_PRECIOS))
+plt.title('Snack favoritos')
+plt.xlabel('Precios')
+plt.ylabel('Snacks')
+plt.savefig('graficoSnackFav.png')
+plt.show()
+
 
