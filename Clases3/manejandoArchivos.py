3019e3411dadf2be8d426e8b4638b97df96a1542
nicollebanos <nicolleb2804@gmail.com>
2021-05-20 07:48:17 -0500

    f

--- Clases3/manejandoArchivos.py
+++ Clases3/manejandoArchivos.py
@@ -7,17 +7,17 @@ estatura = float(input('Ingrese su estatura: '))
 
 nombreArchivo = 'estudiantes.txt'
 try: 
-    archivo = open(nombreArchivo, 'a')
+    archivo = open(nombreArchivo)
     print('1')
 except FileNotFoundError:
     archivo = open(nombreArchivo, 'w', encoding='UTF-8') 
     descripcion = 'Archivo de datos de estudiantes'
-    print(2)
+    print('2')
     archivo.writelines(descripcion)
     sys.exit(1)
 
-archivo = open(nombreArchivo, 'a')
-linea = '\nnombre: '+ nombre + 'edad: '+ str(edad) + 'estatura: '+ str(estatura)
+archivo = open(nombreArchivo,'a')
+linea = "\nnombre:" + nombre + " edad: "+ str(edad) + " estatura: "+ str(estatura)
 archivo.writelines(linea)
 archivo.close()
 
@@ -25,6 +25,11 @@ archivo.close()
 with open(nombreArchivo, 'r') as reader:
     for line in reader:
         print(line)
+
+
+
+
+
 
 
 
