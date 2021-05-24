ca55d2c92b087188aebe05d31c36f2b927ed8b85
nicollebanos <nicolleb2804@gmail.com>
2021-05-24 12:51:05 -0500

    f

---
+++ Talleres/Taller.Final/punto2.py
@@ -0,0 +1,7 @@
+
+# Pida al usuario que ingrese un párrafo y luego muestre en pantalla 
+# cual es la palabra más grande, la palabra más pequeña. Valide que el párrafo
+# ingresado termine en punto sino es así se debe pedir al usuario que ingrese 
+# nuevamente el párrafo
+
+
