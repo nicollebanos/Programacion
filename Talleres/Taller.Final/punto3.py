e2b7dad188d0666c66e99c16cc51b776d69ce858
nicollebanos <nicolleb2804@gmail.com>
2021-05-24 12:52:20 -0500

    f

---
+++ Talleres/Taller.Final/punto3.py
@@ -0,0 +1,7 @@
+
+#  Un taller de biomédica desea tener un archivo para el manejo de los 
+# clientes, se pide que desarrolle un programa que en su primera ejecución 
+# cree el archivo llamado mantenimientos.txt. Luego en cada ejecución se 
+# preguntará por el nombre del equipo médico, una descripción y el precio 
+# acordado para el mantenimiento (se deben almacenar estos datos nuevos en 
+# el archivo mantenimientos.txt)
