d682cbef395b7a70ae91a03b917a7248b4e28ed5
nicollebanos <nicolleb2804@gmail.com>
2021-05-25 08:21:04 -0500

    f

--- Talleres/Taller.Final/punto1.py
+++ Talleres/Taller.Final/punto1.py
@@ -10,14 +10,9 @@ PREGUNTA_NOMBRE = 'Digite su nombre: '
 PREGUNTA_ESTATURA = 'Digita tu estatura: '
 MENSAJE_BIENVE = 'Hola, calculare tu IMC, llena los siguentes datos'
 MENSAJE_DESPEDIDA = 'Tú IMC es... '
-MENSAJE_BAJO_PESO = 'Tas delgado'
-MENSAJE_NORMAL = 'Te encuentras en forma'
-MENSAJE_SOBRE_PESO = 'Abre el ojo, estas en sobre peso'
-MENSAJE_OBESO = 'Tú salud corre peligro, tate quieto estas obeso'
 
 #--------------constantes-----------#
-print(MENSAJE_BIENVE)
-
+print(MENSAJE_BIENVE)            
 
 isCorrectInfo = False
 while(isCorrectInfo == False):
@@ -46,17 +41,4 @@ while(isCorrectInfo == False):
 
 
 imc = peso/(estatura**2)
-isBajoPeso = imc < 18.5 
-isNormal = imc >= 18.5 and imc < 25
-isSobrePeso = imc > 25 and imc < 30
-resultado = ""
-if(isBajoPeso):
-    resultado = MENSAJE_BAJO_PESO
-elif(isNormal):
-    resultado = MENSAJE_NORMAL
-elif(isSobrePeso):
-    resultado = MENSAJE_SOBRE_PESO
-else:
-    resultado = MENSAJE_OBESO
 print(MENSAJE_DESPEDIDA, imc)
-print(resultado)
--- Talleres/Taller.Final/punto3.py
+++ Talleres/Taller.Final/punto3.py
@@ -1,7 +1,32 @@
 
-#  Un taller de biomédica desea tener un archivo para el manejo de los 
+# Un taller de biomédica desea tener un archivo para el manejo de los 
 # clientes, se pide que desarrolle un programa que en su primera ejecución 
 # cree el archivo llamado mantenimientos.txt. Luego en cada ejecución se 
 # preguntará por el nombre del equipo médico, una descripción y el precio 
 # acordado para el mantenimiento (se deben almacenar estos datos nuevos en 
 # el archivo mantenimientos.txt)
+
+def validarArchivo(nombreArchivo, descripcion):
+    try:
+        archivo = open(nombreArchivo)
+        return True
+    except FileNotFoundError:
+        archivo = open(nombreArchivo, 'w', encoding='UTF-8')
+        print("2")
+        archivo.writelines(descripcion)
+        return False
+
+def guardarLinea (nombreArchivo, lineaIn):
+    archivo = open(nombreArchivo,'a')
+    archivo.writelines(lineaIn)
+
+nameFile = "mantenimientos.txt"
+isValidate = validarArchivo(nameFile, 'Seguimiento de mantenimiento de equipos medicos')
+if (isValidate):
+    descEquipo = input('Ingrese la descripción del equipo : ')
+    nombre = validateString('Ingrese su nombre :')
+    precio = validateFloat('Ingrese el precio : ')
+    linea ='\nDescripcion465'+ descEquipo+ ' nombre técnico: ' + nombre + ' precio acordado: '+ str(precio)
+    guardarLinea(nameFile, linea)
+else:
+    print('se creó el archivo')
