164c4c6079b3c82cc074cd60d401b58a8a3b4937
nicollebanos <nicolleb2804@gmail.com>
2021-05-27 06:08:14 -0500

    f

---
+++ examenes/examenFinal/punto1.py
@@ -0,0 +1,22 @@
+
+# Solicite a un usuario que ingrese sus 8 alimentos favoritos y sus precios 
+# luego realice un gráfico de barras con la información ingresada (recuerde poner título al 
+# gráfico y a sus ejes también recuerde guardar el resultado en un archivo png)
+
+import matplotlib.pyplot as plt
+PREGUNTA_ALIMEN_FAV = 'Ingrese un alimento favorito: '
+PREGUNTA_PRECIOS = 'Ingrese los precios: '
+alimenFav = []
+precios = []
+for i in range (8):
+    alimen = input(PREGUNTA_ALIMEN_FAV)
+    alimenFav.append(alimen)
+    precio = float(input(PREGUNTA_PRECIOS))
+    precios.append(precio)
+
+plt.bar(alimenFav, precios, width= 0.5, color = 'c')
+plt.title('Alimentos favoritos')
+plt.xlabel('Alimentos')
+plt.ylabel('Precios')
+plt.savefig('graficoAlimenFav.png')
+plt.show()
