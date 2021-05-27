e872c69d00b0c0852dc3988beb400692cf85686e
nicollebanos <nicolleb2804@gmail.com>
2021-05-27 07:48:21 -0500

    F

--- examenes/examenFinal/punto4.py
+++ examenes/examenFinal/punto4.py
@@ -7,4 +7,27 @@
 
 import sys
 
-NOMBRE_ARCHIVO = 'Paciente.txt'
+nombre = input('Digite su nombre: ')
+enfermedad = input('Digite que enfermedad padece: ')
+precio = input('Digite el precio de la consulta: ')
+
+NOMBRE_ARCHIVO = "paciente.txt"
+try:
+    archivo = open(NOMBRE_ARCHIVO)
+    print('I')
+except FileNotFoundError: 
+    archivo = open (NOMBRE_ARCHIVO, 'w', encoding='UTF-8')
+    descripcion = 'Archivo del paciente de neurología'
+    print('II')
+    archivo.writelines(descripcion)
+    sys.exit(1)
+
+archivo = open(NOMBRE_ARCHIVO,'a')
+linea = "\nnombre:" + nombre + " edad: "+ str(enfermedad) + " estatura: "+ str(precio)
+archivo.writelines(linea)
+archivo.close()
+
+with open(NOMBRE_ARCHIVO, 'r') as reader:
+    for line in reader:
+        print(line)
+
